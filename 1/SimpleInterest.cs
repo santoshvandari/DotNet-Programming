28b0f069c661f6cf963ccf0feb82858e30932a35
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-28 07:17:01 +0545

    initial

---
+++ 1/SimpleInterest.cs
@@ -0,0 +1,11 @@
+class SimpleInterest{
+    public void Main(){
+        Console.Write("Enter Princial: ");
+        int p=Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Time: ");
+        int t=Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Rate: ");
+        float r = Convert.ToDouble(Console.WriteLine());
+
+    }
+}
