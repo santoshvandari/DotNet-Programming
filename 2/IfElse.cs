48cce7a35fba1fdb7bfa082936b92f286d33d402
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-29 07:16:59 +0545

    Initial

---
+++ 2/IfElse.cs
@@ -0,0 +1,18 @@
+using System;
+class IfElse{
+    static void Main(){
+        int num;
+        Console.Write("Enter a Number: ");
+        num=Convert.ToInt32(Console.ReadLine());
+        if(num%3 ==0 && num%7==0){
+            Console.WriteLine("{0} is Divisible By 3 and 7",num);
+        }else if (num%3==0){
+            Console.WriteLine("{0} is Divisible By 3",num);
+        }else if (num%7==0){
+            Console.WriteLine("{0} is Divisible By 7",num);
+        }else{
+            Console.WriteLine("{0} Not Divisioble by 3 and 7.",num)
+        }
+
+    }
+}
