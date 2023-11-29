9707c2c805d6b36de1fc89a096fb3cde3e21c285
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-29 18:19:09 +0545

    Initial

---
+++ 3/Array.cs
@@ -0,0 +1,8 @@
+using System;
+class Array{
+    static void Main(){
+        int[] arr={10,20,50,45,045,46,5,65,54,65,5,6565};
+        for(int i=0;i<arr.Length;i++)
+            Console.WriteLine(arr[i]);
+        }
+}
