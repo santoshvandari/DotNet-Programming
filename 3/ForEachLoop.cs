a46ec66066eddb42806c0374854529299a452910
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-29 18:20:27 +0545

    Initial

---
+++ 3/ForEachLoop.cs
@@ -0,0 +1,8 @@
+using System;
+class ForEachLoop{
+    static void Main(){
+        int[] arr={10,20,50,45,045,46,5,65,54,65,5,6565};
+        foreach(int data in arr)
+            Console.WriteLine(data);
+        }
+}
