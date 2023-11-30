be348f11fcea48ea12e77a1fab7b13065d031aa4
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-30 07:17:37 +0545

    Initial

--- 2/IfElse.cs
+++ 2/IfElse.cs
@@ -1,6 +1,12 @@
 using System;
 class IfElse{
     static void Main(){
-        
+        Console.WriteLine("Enter Your Age: ");
+        int age=Convert.ToInt32(Console.ReadLine());
+        if(age>17){
+            Console.WriteLine("You are Eligible for Voting...");
+        }else{
+            Console.WriteLine("You are not Eligible for Voting...");
+        }
     }
 }
