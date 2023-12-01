f4042fc297b19c34100ee211a52e2b5fd333be2d
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-01 13:22:22 +0545

    initial

--- 4/RectangularArray2.cs
+++ 4/RectangularArray2.cs
@@ -6,11 +6,20 @@ class RectangularArray2{
             {4,5,6},
             {7,8,9}
         };
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                Console.Write(arr[i,j]+"\t");
+        // for(int i=0;i<3;i++){
+        //     for(int j=0;j<3;j++){
+        //         Console.Write(arr[i,j]+"\t");
+        //     }
+        //     Console.WriteLine();
+        // }
+        int counter=0;
+        foreach (int item in arr){
+            Console.Write(item+"\t");
+            counter++;
+            if(counter==3){
+                Console.WriteLine();
+                counter=0;
             }
-            Console.WriteLine();
         }
     }
 }
