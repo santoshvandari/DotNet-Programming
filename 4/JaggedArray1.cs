b7112eb8a9d57f8e9ef19ce776769a19465b7c1c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-01 13:37:12 +0545

    initial

---
+++ 4/JaggedArray1.cs
@@ -0,0 +1,16 @@
+using System;
+class JaggedArray1{
+    static void Main(){
+        int [][] arr={
+            new int[]{1,2,3},
+            new int[]{4,5},
+            new int[]{6,7,8,9}
+        }
+        for(int i=0;i<arr.Length;i++){
+            for(int j=0;j<arr[i].Length;j++){
+                Console.Write(arr[i][j]+"\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
