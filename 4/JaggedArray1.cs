6c86815d27d13f242e66c0732e7856f0631f2114
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-01 13:41:54 +0545

    initial

--- 4/JaggedArray1.cs
+++ 4/JaggedArray1.cs
@@ -5,10 +5,17 @@ class JaggedArray1{
             new int[]{1,2,3},
             new int[]{4,5},
             new int[]{6,7,8,9}
-        }
-        for(int i=0;i<arr.Length;i++){
-            for(int j=0;j<arr[i].Length;j++){
-                Console.Write(arr[i][j]+"\t");
+        };
+        // for(int i=0;i<arr.Length;i++){
+        //     for(int j=0;j<arr[i].Length;j++){
+        //         Console.Write(arr[i][j]+"\t");
+        //     }
+        //     Console.WriteLine();
+        // }
+
+        foreach(int[] ar in arr){
+            foreach(int item in ar){
+                Console.Write(item+"\t");
             }
             Console.WriteLine();
         }
---
+++ 4/tempCodeRunnerFile.cs
@@ -0,0 +1,6 @@
+foreach(int[] ar in arr){
+            foreach(int item in ar){
+                Console.Write(item+"\t");
+            }
+            Console.WriteLine();
+        }
