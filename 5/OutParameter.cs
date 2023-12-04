61ac94df4334cc3c2ebdac984739ac0a2ac06038
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-04 14:17:04 +0545

    Initial

--- 5/OutParameter.cs
+++ 5/OutParameter.cs
@@ -1,6 +1,13 @@
 using System;
 class OutParameter{
+    static void Test(int x,int y, out int sum){
+        sum = x+y;
+    }
     static void Main(){
-        
+        int sum,res;
+        Test(10,5,out sum);
+        Test(10,10,out res);
+        Console.WriteLine($"Sum : {sum}");
+        Console.WriteLine($"Sum : {res}");
     }
 }
