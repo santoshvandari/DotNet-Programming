8ba88e6c40baf0414c6e904fd9142ec00df83919
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-04 14:37:12 +0545

    Initial

---
+++ 6/ReferenceType.cs
@@ -0,0 +1,25 @@
+using System;
+class ReferenceType{
+    static void Testing(int[] arr){
+        arr[3]=60;
+    }
+    static void Test2(string name){
+        name='Hari';
+    }
+    static void Main(){
+        int[] arr={10,20,30,40,50};
+        foreach (int item in arr){
+            Console.Write($"{item}\t");            
+        }
+        Testing(arr);
+        Console.WriteLine();
+        foreach (int item in arr){
+            Console.Write($"{item}\t");            
+        }
+        string name="Santosh";
+        Console.WriteLine(name);
+        Test2(name);
+        Console.WriteLine(name);
+
+    }
+}
