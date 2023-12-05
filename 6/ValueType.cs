4312ee03f2f679bd4161052f72753e3e5a6d973d
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-05 07:25:26 +0545

    Initial

--- 6/ValueType.cs
+++ 6/ValueType.cs
@@ -2,6 +2,11 @@ using System;
 class ValueType{
     static void Main(){
         int a=10;
+        Console.WriteLine(a);
         Test(a);
+        Console.WriteLine(a);
+    }
+    static void Test(int a){
+        a+=10;
     }
 }
