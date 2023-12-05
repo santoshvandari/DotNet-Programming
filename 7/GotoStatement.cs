b55d447afc79dd8064112d4e373c29fba7a3fce5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-05 14:15:02 +0545

    Initial

--- 7/GotoStatement.cs
+++ 7/GotoStatement.cs
@@ -1,6 +1,14 @@
 using System
 class GotoStatement{
     static void Main(){
+        label:
+        for(int i=0;i<5;i++){
+            Console.WriteLine("i= {0}",i);
+        }
+        Console.WriteLine("Do You Want to Run Again(y/n):");
+        char ch = Console.ReadLine();
+        if(ch=='y')
+            goto label;
         
     }
 }
