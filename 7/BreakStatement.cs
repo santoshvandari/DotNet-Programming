a1d6e32a6713d6d5e788febba8d1c5be4b09de1a
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-05 14:06:31 +0545

    Initial

--- 7/BreakStatement.cs
+++ 7/BreakStatement.cs
@@ -1,6 +1,11 @@
 using System;
 class BreakStatement{
     static void Main(){
-        
+        for(int i=0;i<10;i++){
+            if(i==4){
+                break;
+            }
+            Console.WriteLine(i);
+        }
     }
 }
