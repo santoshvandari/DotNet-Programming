8a55cdde25b0284a47171af06493286b9eb8bb59
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-05 14:08:01 +0545

    Initial

--- 7/ContinueStatement.cs
+++ 7/ContinueStatement.cs
@@ -1,6 +1,11 @@
-using System
+using System;
 class ContinueStatement{
     static void Main(){
-        
+        for(int i=0;i<10;i++){
+            if(i==4){
+                continue;
+            }
+            Console.WriteLine(i);
+        }
     }
 }
