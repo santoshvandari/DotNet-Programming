5a522553597053e91e91ec33cf8f910a9e9bbc2c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-09 07:56:27 +0545

    Initial

---
+++ 7/ExitStatement.cs
@@ -0,0 +1,12 @@
+using System;
+class ExitStatement{
+    static void Main(){
+        for(int i=0;i<=10;i++){
+            for(int j=0;j<=10;j++){
+                Console.WriteLine("{0}:{1}",i,j);
+                if(i==5 && j==5)
+                    exit;
+            }
+        }
+    }
+}
