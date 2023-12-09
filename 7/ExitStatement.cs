4b89e63107e07a8c150e36edca5d75d1f91fb7c0
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-09 07:59:42 +0545

    Initial

--- 7/ExitStatement.cs
+++ 7/ExitStatement.cs
@@ -4,8 +4,10 @@ class ExitStatement{
         for(int i=0;i<=10;i++){
             for(int j=0;j<=10;j++){
                 Console.WriteLine("{0}:{1}",i,j);
-                if(i==5 && j==5)
-                    Exit(1);
+                if(i==5 && j==5){
+                    Console.WriteLine("Exiting...");
+                    Environment.Exit(1);
+                }
             }
         }
     }
