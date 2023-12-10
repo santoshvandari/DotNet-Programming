40e6b5145aec29946891364d7ea270b614dc86f7
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-10 17:16:21 +0545

    Initial

--- 9/Delegates1.cs
+++ 9/Delegates1.cs
@@ -1,6 +1,16 @@
 using System;
 class Delegates1{
+    delegate void Msg();
+    void Message1(){
+        Console.WriteLine("Welcome to C#");
+    }
+    static void Message2(){
+        Console.WriteLine("WELCOME TO C#");
+    }
     static void Main(){
-        
+        Delegates1 obj= new Delegates1();
+        Msg del = new Msg(obj.Message1);
+        del=del+Message2;
+        del();
     }
 }
