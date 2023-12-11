a2bf1463fca819eef3de47ede080ef6dae001fb6
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-11 14:20:23 +0545

    Initial

--- 10/FunctionDelegates.cs
+++ 10/FunctionDelegates.cs
@@ -1,7 +1,11 @@
 using System;
 class FunctionDelegates{
-    
+    static int Add(int x, int y ){
+        return (x+y);
+    }
     static void Main(){
+        Func<int,int,int> del = Add;
+        Console.WriteLine("Sum: {0}",del(20,10));
 
     }
 }
