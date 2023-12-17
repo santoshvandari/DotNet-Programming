4cab874c5b60f66ffaf8e9ba5922b15f59d6b071
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-17 14:17:31 +0545

    Initiial

--- 11/StatementLambda.cs
+++ 11/StatementLambda.cs
@@ -1,7 +1,11 @@
 using System;
 class StatementLambda{
-    
+    public delegate void MyDel(int x, int y);    
     static void Main(){
-
+        MyDel del =(x,y)=>{
+            Console.WriteLine("Sum : {0}",(x+y));
+            Console.WriteLine("Difference : {0}",(x-y));
+        };
+        del(10,20);
     }
 }
