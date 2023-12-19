547d4f8c3624b4508071993fa33ab7a6633f6ece
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-19 14:22:37 +0545

    InitiaiL

--- 12/ArithmeticException.cs
+++ 12/ArithmeticException.cs
@@ -1,4 +1,11 @@
 using System;
 class ArithmeticException{
-    
+    static void Main(){
+        try{
+            
+        }catch(ArithmeticException ex){
+            Consoe.WriteLine("Error: {0}", ex.Message);
+        }
+
+    }
 }
