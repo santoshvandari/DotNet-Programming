719ff9118e6bee81be9f313a549458fcda3f2697
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-19 14:27:49 +0545

    InitiaiL

---
+++ 12/IndexOutOfRangeEx.cs
@@ -0,0 +1,12 @@
+using System;
+class IndexOutOfRangeEx{
+    static void Main(){
+        int[] arr=new int[2];
+        try{
+            arr[0]=10;
+            arr[2]=3;
+        }catch(IndexOutOfRangeException ex){
+            Console.WriteLine(ex);
+        }
+    }
+}
