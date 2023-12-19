dd36349be35fcfc58a369eccf897a83d00e99eb5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-19 14:34:43 +0545

    InitiaiL

--- 12/NullReferenceExceptionEx.cs
+++ 12/NullReferenceExceptionEx.cs
@@ -2,8 +2,10 @@ using System;
 class NullReferenceExceptionEx{
     static void Main(){
         string str="Santosh";
+        string str1=null;
         try{
             Console.WriteLine(str.ToUpper());
+            Console.WriteLine(str1.ToUpper());
         }catch(NullReferenceException ex){
             Console.WriteLine(ex);
         }
