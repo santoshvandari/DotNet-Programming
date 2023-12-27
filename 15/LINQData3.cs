716a5b5ed0fcca9908a0325468e0e43f160a9e68
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-27 17:46:05 +0545

    initial

--- 15/LINQData3.cs
+++ 15/LINQData3.cs
@@ -4,7 +4,7 @@ using System.Linq;
 class Student{
     public int sid,age;
     public string name,address;
-    Student(int sid, string name, string address, int age){
+    public Student(int sid, string name, string address, int age){
         this.sid=sid;
         this.name=name;
         this.address=address;
@@ -13,6 +13,18 @@ class Student{
 }
 class LINQData3{
     static void Main(){
+        List<Student> list = new List<Student>();
+        list.Add(new Student(100,"Santosh","KTM",25));
+        list.Add(new Student(101,"Ayush","BTM",22));
+        list.Add(new Student(102,"Anish","BDP",23));
+        list.Add(new Student(103,"Kushal","BTM",25));
+        list.Add(new Student(104,"Manoj","KTM",28));
+        list.Add(new Student(105,"Kiran","KVT",22));
+
+        Console.WriteLine("Sid\tName\tAddress\tAge");
+        foreach(var data in list){
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}",data.sid,data.name,data.address,data.age);
+        }
 
     }
 }
