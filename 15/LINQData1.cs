f60b933841589a358b5ac900f6c5cf6e76b86e65
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-27 14:09:39 +0545

    Initial

---
+++ 15/LINQData1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+class Employee{
+    public int eid,age;
+    public string name,address;
+    public double salary;
+}
+class LINQData1{
+    static void Main(){
+        List<Employee> list = new List<Employee>();
+        
+    }
+}
