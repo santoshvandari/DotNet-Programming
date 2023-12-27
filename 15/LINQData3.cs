e1f7cdbab3f56b714b12dd7bcca04cbc0848a49a
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-27 17:38:48 +0545

    initial

--- 15/LINQData3.cs
+++ 15/LINQData3.cs
@@ -1,1 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+class Student{
+    public int sid,age;
+    public string name,address;
+    Student(int sid, string name, string address, int age){
+        this.sid=sid;
+        this.name=name;
+        this.address=address;
+        this.age=age;
+    }
+}
+class LINQData3{
+    static void Main(){
+
+    }
+}
