6454035d0f16ee7089ab2bfffebc7230d4bc796e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-30 16:05:04 +0545

    initial

--- Labsheet/11/MultiCastingDelegates.cs
+++ Labsheet/11/MultiCastingDelegates.cs
@@ -1,1 +1,20 @@
 // Write a C# program to demonstrate multicasting of delegates.
+using System;
+class MultiCastingDelegates{
+    delegate void MultiCast();
+    static void Method1(){
+        Console.WriteLine("Method 1");
+    } 
+    static void Method2(){
+        Console.WriteLine("Method 2");
+    }
+    static void Method3(){
+        Console.WriteLine("Method 3");
+    }
+    static void Main(){
+        MultiCast obj = new MultiCast(Method1);
+        obj += Method2;
+        obj += Method3;
+        obj();
+    }
+}
