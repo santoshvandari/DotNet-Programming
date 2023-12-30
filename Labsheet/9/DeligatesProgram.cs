36b454aab6969e737b070fba736b21d81061dcf6
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-30 15:59:31 +0545

    initial

---
+++ Labsheet/9/DeligatesProgram.cs
@@ -0,0 +1,16 @@
+// Write a C# program to find sum and product of two numbers using delegate.
+using System;
+class DelegatesProgram{
+    public delegate void Calculation(int x, int y);
+    static void Sum(int a, int b){
+        Console.WriteLine("Sum: {0}", a+b);
+    }
+    static void Product(int x, int y){
+        Console.WriteLine("Product: {0}", x*y);
+    }
+    static void Main(){
+        Calculation calc=new Calculation(Sum);
+        calc+=Product; 
+        calc(10, 20);
+    }
+}
