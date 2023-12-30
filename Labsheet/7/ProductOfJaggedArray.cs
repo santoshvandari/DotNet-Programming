4afb57d49a9342a6bea1a9874e63d48e6a83ce73
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-30 14:40:56 +0545

    initial

--- Labsheet/7/ProductOfJaggedArray.cs
+++ Labsheet/7/ProductOfJaggedArray.cs
@@ -2,13 +2,14 @@
 using System;
 class ProductOfJaggedArray{
     static void Main(){
-        int[][] arr = new int[3][];
-        arr[0] = new int[]{1,2,3,4};
-        arr[1] = new int[]{5,6,7};
-        arr[2] = new int[]{8,9};
-        foreach(int[] i in arr){
+        int[][] arr ={
+            new int[]{1,2,3,4},
+            new int[]{5,6,7},
+            new int[]{8,9},
+        };
+        foreach(int[] ar in arr){
             int product = 1;
-            foreach(int num in i)
+            foreach(int num in ar)
                 product *= num;
             Console.WriteLine("Product : {0}",product);
         }
