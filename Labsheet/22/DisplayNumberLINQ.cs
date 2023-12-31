f3c5f7174d49608ee9e3ee65592242b5369d2ffb
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-31 09:57:28 +0545

    INitial

---
+++ Labsheet/22/DisplayNumberLINQ.cs
@@ -0,0 +1,16 @@
+// Write a program in C# Sharp to create a list of numbers and display the numbers greater than 80 as output using LINQ. Also display all selected numbers in descending order.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class DisplayNumberLINQ{
+    static void Main(){
+        List<int> list = new List<int>(10,20,30,80,180,470,48,48,6958,65,8,69,55,5,14,65,4,15,21,1,58,4,65,4,8);
+        var filteredData=from x in list
+                            where x > 80
+                            orderby x descending
+                            select x; 
+        Console.WriteLine("Numbers greater than 80 are:");   
+        foreach(var x in filteredData)
+            Console.Write(x+"\t");
+    }
+}
