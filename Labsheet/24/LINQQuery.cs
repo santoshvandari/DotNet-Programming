70c2eca112443a5bd153bb9643b27c557fb13370
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 18:22:32 +0545

    Updated

--- Labsheet/24/LINQQuery.cs
+++ Labsheet/24/LINQQuery.cs
@@ -29,15 +29,24 @@ class LINQQuery1{
         list.Add(new Employee(3,"Ravi",35,"BTM",40000));
         list.Add(new Employee(4,"Rahul",40,"BRT",50000));
         list.Add(new Employee(5,"Krishna",45,"BTM",30000));
-        //Select name and age of employees whose salary is greater than 20000 and age is less than 35.
-        var res1 = from x in list
-                    where x.salary>20000 && x.age<35
-                    select new{x.name,x.age};
-        Console.WriteLine("Name And Age of Employees whose salary is greater than 20000 and age is less than 35");
-        foreach (var item in res1){
-            Console.WriteLine("{0}\t{1}",item.name,item.age);
-        }
-        // Select all records of employees whose name starts with letter ‘R’ and age greater than 25 in descending order based on age.
+        // //Select name and age of employees whose salary is greater than 20000 and age is less than 35.
+        // var res1 = from x in list
+        //             where x.salary>20000 && x.age<35
+        //             select new{x.name,x.age};
+        // Console.WriteLine("Name And Age of Employees whose salary is greater than 20000 and age is less than 35");
+        // foreach (var item in res1){
+        //     Console.WriteLine("{0}\t{1}",item.name,item.age);
+        // }
+        // // Select all records of employees whose name starts with letter ‘R’ and age greater than 25 in descending order based on age.
+        // var res2 = from x in list
+        //             where x.name.StartsWith("R") && x.age>25
+        //             orderby x.age descending
+        //             select x;
+        // Console.WriteLine("Records of Employee whose name starts with letter ‘R’ and age greater than 25 in descending order based on age:");
+        // foreach (var item in res2){
+        //     Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}",item.id,item.name,item.age,item.address,item.salary);
+        // }
+
 
 
     }
