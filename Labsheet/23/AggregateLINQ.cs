7403263443bcd9e4f1d47ad412c7433890a0bdcc
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 17:55:44 +0545

    Updated

--- Labsheet/23/AggregateLINQ.cs
+++ Labsheet/23/AggregateLINQ.cs
@@ -3,6 +3,15 @@ using System;
 using System.Linq;
 class AggregateLINQ{
     static void Main(string[] args){
-        
+        int[] arr={4,8,10,48,17,46,79,48,15,20};
+        var res=from x in arr
+            where x>(
+                from y in arr
+                select y
+            ).Average()
+            select x;
+        foreach (int data in res){
+            Console.WriteLine(data);
+        }
     }
 }
