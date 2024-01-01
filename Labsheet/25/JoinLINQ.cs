cc27e11cd611afff7db227969fc02e403306fb92
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 18:01:55 +0545

    Updated

--- Labsheet/25/JoinLINQ.cs
+++ Labsheet/25/JoinLINQ.cs
@@ -2,6 +2,24 @@
 using system;
 using System.Linq;
 using System.Collections.Generic;
+class Student{
+    int sid,cid;
+    string sname,address;
+    public Student(int sid,string sname,string address,int cid){
+        this.sid=sid;
+        this.sname=sname;
+        this.address=address;
+        this.cid=cid;
+    }
+}
+class Course{
+    int cid;
+    string cname;
+    public Course(int cid,string cname){
+        this.cid=cid;
+        this.cname=cname;
+    }
+}
 class JoinLINQ{
     static void Main(){
         
