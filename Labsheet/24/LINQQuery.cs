731ea641334790c6a70950eab00985c30a76f3cc
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 18:20:11 +0545

    Updated

--- Labsheet/24/LINQQuery.cs
+++ Labsheet/24/LINQQuery.cs
@@ -10,9 +10,9 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 class Employee{
-    int id,age;
-    double salary;
-    string name,address;
+    public int id,age;
+    public double salary;
+    public string name,address;
     public Employee(int id,string name,int age,string address,double salary){
         this.id=id;
         this.name=name;
@@ -23,8 +23,8 @@ class Employee{
 }
 class LINQQuery1{
     static void Main(){
-        List<Employee> list = new Employee<Employee>();
-        list.Add(new Employee(1,"ROhit",25,"KTM",20000));
+        List<Employee> list = new List<Employee>();
+        list.Add(new Employee(1,"Rohit",25,"KTM",20000));
         list.Add(new Employee(2,"Raj",30,"KVT",50000));
         list.Add(new Employee(3,"Ravi",35,"BTM",40000));
         list.Add(new Employee(4,"Rahul",40,"BRT",50000));
@@ -33,10 +33,11 @@ class LINQQuery1{
         var res1 = from x in list
                     where x.salary>20000 && x.age<35
                     select new{x.name,x.age};
-        
-        foreach (var item in collection){
-            
+        Console.WriteLine("Name And Age of Employees whose salary is greater than 20000 and age is less than 35");
+        foreach (var item in res1){
+            Console.WriteLine("{0}\t{1}",item.name,item.age);
         }
+        // Select all records of employees whose name starts with letter ‘R’ and age greater than 25 in descending order based on age.
 
 
     }
