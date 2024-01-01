96e1192e47d6333c8302d52c8f82bd26eb0c860a
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 14:15:27 +0545

    Initial

--- Practice/16/AggregateFunction1.cs
+++ Practice/16/AggregateFunction1.cs
@@ -1,6 +1,8 @@
+// Select Records of Students whose age is Maximum among all the Students
+// Select Name and Address of Employees whose salary is greater than average salary of all employees
 using system;
 class AggregateFunction1{
     static void Main(){
-        
+
     }
 }
