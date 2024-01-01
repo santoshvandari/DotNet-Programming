1d1bd9fe262267d0ee7fea8bdec30570df1a1ed6
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 12:10:18 +0545

    Initial

---
+++ Labsheet/23/AggregateLINQ.cs
@@ -0,0 +1,8 @@
+// Write a C# program to demonstrate aggregate functions using LINQ.
+using System;
+using System.Linq;
+class AggregateLINQ{
+    static void Main(string[] args){
+        
+    }
+}
---
+++ Labsheet/24/LINQQuery.cs
@@ -0,0 +1,16 @@
+/* Write a C# program to store id, name, age, address and salary of 5 employees in a list.
+Now write LINQ query for the following:
+• Select name and age of employees whose salary is greater than 20000 and age is less than 35.
+• Select all records of employees whose name starts with letter ‘R’ and age greater than 25 in descending order based on age.
+• Select all records of employee whose salary is maximum.
+• Select id and name of employee whose salary is between 20000 and 30000.
+• Select id and name of employees whose address is Btm and salary is greater than average salary of employees.
+• Select records of employee group by address in ascending order based on age. */
+using System;
+using System.Linq;
+using System.Collections.Generic;
+class LINQQuery{
+    static void Main(){
+        
+    }
+}
