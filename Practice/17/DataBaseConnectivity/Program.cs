ecfbf43976cc92d270061b0bbe6945e3b486a8ef
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-07 16:07:47 +0545

    Initial

--- Practice/17/DataBaseConnectivity/Program.cs
+++ Practice/17/DataBaseConnectivity/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MySql.Data.Client;
+class Program{
+    static void Main(){
+        string connectingstring = "SERVER=localhost;"
+    }
+}
