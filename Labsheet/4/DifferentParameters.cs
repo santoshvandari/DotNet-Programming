2bd3a02eb4dd58ff68ebe0c9ecbdcd188c1f61fa
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-07 16:03:29 +0545

    Initial

--- Labsheet/4/DifferentParameters.cs
+++ Labsheet/4/DifferentParameters.cs
@@ -2,13 +2,13 @@
 using System;
 class DifferentParameters{
     static void Reference(ref int a){
-        Console.WriteLine("By Reference : ",a);
+        Console.WriteLine("By Reference : {0}",a);
     }
-    static void Output(int a, int b,out int sum){
+    static void OutPara(int a, int b,out int sum){
         sum = a + b;
     }
     static void Params(params int[] a){
-        Console.Write("Params :\t");
+        Console.Write("Params : ");
         foreach(int i in a){
             Console.Write("\t"+i);
         }
@@ -19,7 +19,7 @@ class DifferentParameters{
     static void Main(){
         int a = 10,sum=0;
         Reference(ref a);
-        Output(10,20,out sum);
+        OutPara(10,20,out sum);
         Console.WriteLine("Sum : {0}",sum);
         Params(1,2,3,4,5);
         Optional(10);
