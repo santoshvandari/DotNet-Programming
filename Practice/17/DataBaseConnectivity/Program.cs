0f45a4f47a0ab830e20ce19705508ad3a4a0efbd
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-07 16:12:40 +0545

    Initial

--- Practice/17/DataBaseConnectivity/Program.cs
+++ Practice/17/DataBaseConnectivity/Program.cs
@@ -2,6 +2,17 @@
 using MySql.Data.Client;
 class Program{
     static void Main(){
-        string connectingstring = "SERVER=localhost;"
+        string connectingstring = "SERVER=localhost;DATABASE=BCA;UID=root;PASSWORD=;";
+        MySqlConnection conn= new MySqlConnection(connectingstring);
+        conn.Open();
+        MySqlCommand cmd = new MySqlCommand();
+        string query="CREATE TABLE IF NOT EXISTS Student(roll INT, name VARCHAR(50),address VARCHAR(50));";
+        cmd.ExecuteNonQuery(query,conn);
+        Console.WriteLine("Table Created Successfully");
+        string insert = "INSERT INTO STUDENT VALUES(100,'Santosh Bhandari','Kanakai');";
+        cmd.ExecuteNonQuery(insert,conn);
+        Console.WriteLine("Data Inserted Successfully");
+        conn.Close();
+
     }
 }
