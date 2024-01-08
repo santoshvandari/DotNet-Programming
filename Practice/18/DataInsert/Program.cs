63ab89bd50179cb1b1898268a1074aa94f5ccd37
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-08 16:52:28 +0545

    Initial

--- Practice/18/DataInsert/Program.cs
+++ Practice/18/DataInsert/Program.cs
@@ -4,10 +4,12 @@ class Program{
         string connection ="SERVER=localhost;DATABASE=BCA;UID=root;PASSWORD=;";
         MySqlConnection conn = new MySqlConnection(connection);
         conn.Open();
-        string query = "INSERT INTO Student VALUES(101,'Aryan','Kanakai";
+        // string query = "INSERT INTO Student VALUES(101,'Aryan','Kanakai');";
+        string query = "UPDATE Student set name = 'San',address='Birtamode' WHERE roll=101";
         MySqlCommand cmd = new MySqlCommand(query,conn);
         cmd.ExecuteNonQuery();
-        Console.WriteLine("Data Inserted");
+        // Console.WriteLine("Data Inserted");
+        // Console.WriteLine("Data Inserted");
         conn.Close();
     }
 }
