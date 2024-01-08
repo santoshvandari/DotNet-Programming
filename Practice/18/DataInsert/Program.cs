573243f0155a45731cab693ecf3b4b8455487a24
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-08 16:49:33 +0545

    Initial

--- Practice/18/DataInsert/Program.cs
+++ Practice/18/DataInsert/Program.cs
@@ -1,6 +1,13 @@
 ﻿using MySql.Data.MySqlClient;
 class Program{
-    static void Main()[
+    static void Main(){
         string connection ="SERVER=localhost;DATABASE=BCA;UID=root;PASSWORD=;";
-    ]
+        MySqlConnection conn = new MySqlConnection(connection);
+        conn.Open();
+        string query = "INSERT INTO Student VALUES(101,'Aryan','Kanakai";
+        MySqlCommand cmd = new MySqlCommand(query,conn);
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Data Inserted");
+        conn.Close();
+    }
 }
