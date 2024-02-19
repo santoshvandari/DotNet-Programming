e83ee70087439d277c183281df76a3b1481b2722
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-19 09:13:51 +0545

    Initial

--- Practice/21/Properties.cs
+++ Practice/21/Properties.cs
@@ -1,1 +1,15 @@
 using System;
+class Child{
+    private int x;
+    public int data{
+        set{x=value;}
+        get{return x;}
+    }
+}
+class Properties{
+    static void Main(){
+        Child obj = new Child();
+        obj.data=10;
+        Console.WriteLine($"Value of X : {obj.data}");
+    }
+}
