f7a7855fc132d942ccd4d92afacdc0573effcf03
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-19 09:33:37 +0545

    Initial

--- Practice/21/MultiLevelInheritance.cs
+++ Practice/21/MultiLevelInheritance.cs
@@ -4,3 +4,27 @@ class A{
         Console.WriteLine("Class A");
     }
 }
+class B:A{
+    public void Display2(){
+        Console.WriteLine("Class B");
+    }
+}
+class C:B{
+    public void Display3(){
+        Console.WriteLine("Class C");
+    }
+}
+class D:C{
+    public void Display4(){
+        Console.WriteLine("Class D");
+    }
+}
+class MultiLevelInheritance{
+    static void Main(){
+        D obj = new D();
+        obj.Display1();
+        obj.Display2();
+        obj.Display3();
+        obj.Display4();
+    }
+}
