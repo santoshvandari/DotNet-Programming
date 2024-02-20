b404cd564199b7d1f764f2da6a848ee1679556af
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-20 13:13:42 +0545

    Initial

---
+++ Practice/22/OverLoading.cs
@@ -0,0 +1,16 @@
+using System;
+class A{
+    public void Add(int x, int y){
+        Console.WriteLine("Sum : {0}",(x+y));
+    }
+    public void Add(float x, float y){
+        Console.WriteLine("Sum : {0}",(x+y));
+    }
+}
+class OverLoading{
+    public static void Main(){
+        A obj = new A();
+        obj.Add(10,20);
+        obj.Add(10.2,20.2);
+    }
+}
