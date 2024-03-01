46c0203ad7132521fce7ac6e01357105b0c8d9b3
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-03-01 10:38:54 +0545

    Initial

---
+++ Labsheet/Labsheet3/5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Write a C# program to select records of employee whose address is Btm and salary is between 30000 to 40000 using connected architecture (Data Reader).
+
+using System;
+using MySql.Data.MySqlClient;
+
+class Program{
+    static void Main(){
+        string url = "SERVER=localhost;DATABASE=BCA;UID=root;PASSWORD=;";
+        MySqlConnection con = new MySqlConnection(url);
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("SELECT * FROM employee WHERE address='Birtamode' AND salary BETWEEN 30000 AND 40000;", con);
+        MySqlDataReader reader = cmd.ExecuteReader();
+        while(reader.Read()){
+            Console.WriteLine(reader[0] + " " + reader[1] + " " + reader[2] + " " + reader[3]);
+        }
+        con.Close();
+    }
+}
---
+++ Labsheet/Labsheet3/6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Write a C# program to select name and address of employee whose salary is maximum using disconnected architecture. (Data Adapter)
+
+using System;
+using MySql.Data.MySqlClient;
+
+class Program{
+    static void Main(){
+        string url = "SERVER=localhost;DATABASE=BCA;UID=root;PASSWORD=;";
+        MySqlConnection con = new MySqlConnection(url);
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("SELECT * FROM employee WHERE salary = (SELECT MAX(salary) from employee);", con);
+        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+        System.Data.DataSet ds = new System.Data.DataSet();
+        adapter.Fill(ds);
+        for(int i=0; i<ds.Tables[0].Rows.Count; i++){
+            Console.WriteLine(ds.Tables[0].Rows[i][1] + " " + ds.Tables[0].Rows[i][2]);
+        }
+        foreach(row in ds.Tables[0].Rows){
+            Console.WriteLine(row[1] + " " + row[2]);
+        }
+        con.Close();
+    }
+}
