43547a5e58ee40086fbc80607ccb47dbda1cf03c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-03-01 10:13:45 +0545

    Initial

---
+++ Labsheet/Labsheet3/2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Write a C# program to insert records in above employee table.
+
+using System;
+using MySql.Data.MySqlClient;
+
+class Program{
+    static void Main(){
+        string url = "SERVER=localhost;DATABASE=BCA;UID=root;PASSWORD=;";
+        MySqlConnection con = new MySqlConnection(url);
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("INSERT INTO employee(eid,name,address,salary) VALUES(100,'Santosh Bhandari','Birtamode',20000)", con);
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Data Inserted successfully");
+        con.Close();
+    }
+}
